91ebacfd7d6845fabb6720757b597abab9e9ed24
Will Grange <will.grange@gmail.com>
2016-06-30 22:18:53 +0100

    Migrating to emitter

--- run.js
+++ run.js
@@ -4,6 +4,10 @@
 let logger = require('winston')
 let chalk = require('chalk')
 let server = require('./server')
+let ProgressBar = require('progress')
+let async = require('async')
+
+let emitter = require('./lib/emitter')
 
 let time = require('./lib/time')
 let Time = time.Time
@@ -12,7 +16,60 @@ let bench = require('./lib/bench')
 let BenchLoadTests = bench.BenchLoadTests
 
 
+class ProgressManager {
+
+  constructor() {
+    this.bars = {}
+  }
+
+  setup(){
+    return
+    emitter.on('benchTestsStarted', (tests) => {
+      this.bars[tests.name] = new ProgressBar('Running ' + tests.total + ' tests for ' + tests.name + ' [:bar] :percent', {
+        complete: '=',
+        incomplete: '0',
+        width: 80,
+        total: tests.total
+      })
+    })
+
+    emitter.on('benchTestsProgress', (tests) => {
+      this.bars[tests.name].tick()
+    })
+
+    emitter.on('benchTestsFinished', (tests) => {
+      this.bars[tests.name].tick()
+    })
+  }
+}
+
+let progressManager = new ProgressManager().setup()
+
+
 class OutputLogger {
+
+  constructor() {
+    this.setup()
+  }
+
+  setup(){
+    emitter.on('benchTestsFinished', (results) => {
+      let time = new Time(results.totalMilisecs)
+      let resultsArr = [
+        `Time to serve: ${time.seconds()}s.`,
+        `Total endpoints called: ${results.totalTestsRun}.`,
+        `Total errors: ${results.totalErrors}.`
+      ]
+
+      if (results.totalErrors > 0) {
+          logger.info(chalk.black.bold.bgRed(resultsArr.join(' ')))
+          return
+      }
+
+      logger.info(chalk.black.bold.bgGreen(resultsArr.join(' ')))
+    })
+  }
+
   allTestsStarted(args) {
     logger.info(chalk.black.bold.bgYellow(`${args.name} started for ${args.url}. ${args.totalRequests} requests.`))
   }
@@ -30,37 +87,32 @@ class OutputLogger {
   }
 
   benchTestStarting(obj){
-    logger.info(chalk.yellow('request'), {
-      url: obj.url
-    })
-  }
-
-  allTestsComplete(results){
-    let time = new Time(results.totalMilisecs)
-    let resultsArr = [
-      `Time to serve: ${time.seconds()}s.`,
-      `Total endpoints called: ${results.totalTestsRun}.`,
-      `Total errors: ${results.totalErrors}.`
-    ]
-
-    if (results.totalErrors > 0) {
-        logger.info(chalk.black.bold.bgRed(resultsArr.join(' ')))
-        return
-    }
-
-    logger.info(chalk.black.bold.bgGreen(resultsArr.join(' ')))
-  }
-
-  onProgress(progress){
-    logger.info(progress)
+    // logger.info(chalk.yellow('request'), {
+    //   url: obj.url
+    // })
   }
 }
 
 let outputLogger = new OutputLogger()
-let loadTests = new BenchLoadTests({
-  totalRequests: 2,
+
+let loadTests1 = new BenchLoadTests({
+  totalRequests: 50,
   url: 'http://localhost:3001/random-delay',
+  name: 'random delay batch 1',
   observers: [outputLogger]
-}).run()
+})
+
+let loadTests2 = new BenchLoadTests({
+  totalRequests: 50,
+  url: 'http://localhost:3001/random-delay',
+  name: 'random delay batch 2',
+  observers: [outputLogger]
+})
+
+async.eachSeries([loadTests1, loadTests2], (benchTests, done) => {
+  benchTests.run()
+  done()
+})
+
 
 let instance = server.create()
